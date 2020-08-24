cad078d8b93c15fcb7c0696ecb0ce1170b133655
nekoyouknow <neko.youknow@gmail.com>
2020-08-24 14:40:21 +0900

    first

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# neko-complus-sample
-COM+ with .Net Framework 4.7, C# 
+# NekoComPlus -sample
+- COM+ with .Net Framework 4.7
+
+## Step by Step
+![](./README/01.Vs2017-Net47-ClassLib.PNG)
+![](./README/02.Service.PNG)
+![](./README/05.final-result.PNG)
+
+
+
---
+++ Service.cs
@@ -0,0 +1,23 @@
+﻿
+//A
+using System.EnterpriseServices;
+using System.Runtime.InteropServices;
+
+//B
+[assembly: ApplicationName("NEKO")]
+[assembly: ApplicationActivation(ActivationOption.Server)]
+namespace NekoComPlus
+{
+    //C
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+
+    //D : extend ServicedComponent
+    public class Service : ServicedComponent      
+    {
+        public string Hello(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
